02a8c91f6d022b4937c743d9d2814e7c0b36e101
lyamtorres <lyamscott@hotmail.com>
2019-12-06 15:01:48 +0100

    Adminstrator and Network classes added

--- src/Administrator.java
+++ src/Administrator.java
@@ -1,4 +1,90 @@
+package projet;
+
+import java.util.Scanner;
 
 public class Administrator {
+	
+	private String nameAdministrator;
+	private Scanner passWord;
+	protected Network adminNetwork;
+	private Group Normal;
+	
+	
+	Administrator(String Administrator,Network adminNetwork,Group Normal ){
+		
+		this.nameAdministrator=nameAdministrator;
+		
+		this.adminNetwork=adminNetwork;
+		
+		this.Normal=Normal;
+		
+		passWord= new Scanner(System.in);
+		
+	}
+	
+	public String getAdminName() {
+		return this.nameAdministrator;
+	}
+	
+	public Network CreateNetWork () {
+		String nameNetwork ;
+		String netPassWord;
+		Network adminNetwork= new Network (this,nameNetwork,netPassWord);
+	}
+	
+	public String EnterPassWord() {
+		String result=passWord.next();
+		return result; 
+	}
+	
+	public void addWorker(Member Member1 ) {
+		while (getWorkerNumber()< getTaskNumber()) {
+			
+		
+		for (Member i: Worker() ) {
+			for (service j: ServiceList()) {
+				
+				if ((Member[i].Service[j]).equals(Task.Service.name)) {
+					
+					Worker.add(Member[i]);
+				}
+			}
+		}
+		
+	}
+	}
+	
+	public void reduceWorker(int i) {
+		
+		while (getWorkerNumber()>getTaskNumber()) {
+		Worker.remove(i);
+		}
+	}
+	
+	public boolean isTakPossible() {
+	
+		if (Member.getMonney()=Task.getValue()) {
+			if(getWorkerNumber()==getTaskNumber()) {
+				return true;
+			}
+			if (getWorkerNumber()>getTaskNumber()) {
+				
+				reduceWorker(i);
+				
+				return true;
+			
+			}
+			if(getWorkerNumber()<getTaskNumber()) {
+				
+				addWorker();
+				
+				return true;
+			}
+				
+		}else {
+			return false;
+		}
+	}
+	
 
 }
--- src/Network.java
+++ src/Network.java
@@ -1,4 +1,86 @@
-
+package projet;
+import java.lang.reflect.Member;
+import java.util.ArrayList;
 public class Network {
-
+	private String nameNetwork;
+	private String netPassWord;
+	private Administrator modo;
+	private ArrayList<Member>Worker;
+	
+	Network (Administrator modo,String nameNetwork,String netPassWord ){
+		
+		this.nameNetwork=nameNetwork;
+		this.modo=modo;
+		this.netPassWord=netPassWord;
+		
+		Worker = new ArrayList<Member>();
+	}
+	
+	public String getnameNetwork() {
+		return this.nameNetwork;
+	}
+	
+	public Administrator getModo() {
+		return this.modo;
+	}
+	
+	public String getnetPassWord() {
+		return this.netPassWord;
+	}
+	
+	
+	public int getWorkerNumber() {
+		return Worker.size();
+	}
+	
+	public boolean isValidPassWord(String result) {
+		
+		return (result).equals(this.netPassWord);
+	}
+	
+	public void addWorker(Member Member1) {
+		
+		if (isValidPassWord(result)) {
+		
+			while (getWorkerNumber()< getTaskNumber()) {
+			
+		
+		
+				for (Member  i: Worker() ) {
+			
+					for (service j:ServiceList()) {
+				
+						if ((Member[i].Service[j]).equals(Task.Service.name)) {
+					
+					
+							Worker.add(Member[i]);
+				
+						}
+			
+					}
+		
+				}
+		
+	
+			}
+		}else {
+			System.out.println("mot de passe incorrecte");
+		
+		}
+	
+	}
+	
+	public void reduceWorker(Member) {
+		
+		if(isValidPassWord(result)) {
+		
+			while (getWorkerNumber()>getTaskNumber()) {
+		
+				Worker.remove(i);
+		}
+		}else {
+			System.out.println("mot de passe incorrecte");
+		}
+	
+	}
 }
